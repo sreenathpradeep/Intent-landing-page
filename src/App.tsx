00fe2423d7cbc9a0597c55b14634c8f7dbdb8a9a
sreenath <sreenathpradeep@gmail.com>
2025-06-05 17:06:02 +0530

    removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/next";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
@@ -22,7 +21,6 @@ function App() {
       </main>
       <Footer />
       <FloatingCTA />
-      <Analytics />
     </div>
   );
 }
